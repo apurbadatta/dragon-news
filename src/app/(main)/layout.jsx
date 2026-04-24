53ae89e12eeb668989f3710db1f5c00e7ac931f7
apurbadatta <apurbadatta76761@gmail.com>
2026-04-24 22:40:54 +0600

    nave ber design

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,3 +1,4 @@
+import BreakingNews from '@/components/shared/BreakingNews';
 import Header from '@/components/shared/Header';
 import Navber from '@/components/shared/Navber';
 import React from 'react';
@@ -7,6 +8,7 @@ const MainLayout = ({children}) => {
         <>
 
         <Header></Header>
+        <BreakingNews></BreakingNews>
         <Navber></Navber>
          {children}
 
