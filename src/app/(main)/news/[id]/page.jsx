699ebda071dbdb821daf5edba0016f90e7785603
apurbadatta <apurbadatta76761@gmail.com>
2026-04-24 19:19:08 +0600

    first commit

---
+++ src/app/(main)/news/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsDetailsPage = () => {
+    return (
+        <div>
+            Newsdetails page
+        </div>
+    );
+};
+
+export default NewsDetailsPage;
